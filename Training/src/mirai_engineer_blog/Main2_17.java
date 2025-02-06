59f4d4f385d21d65c9ebf361e3728deb2f7e7d1e
KAMIMURA SHUN <miniaturebulb470@gmail.com>
2025-02-06 15:04:14 +0900

    a

---
+++ Training/src/mirai_engineer_blog/Main2_17.java
@@ -0,0 +1,26 @@
+package mirai_engineer_blog;
+
+public class Main2_17 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+class GasolineException extends Exception{
+	public GasolineException(String s) {
+		super(s);
+	}
+}
+class Car{
+	int number;
+	String type;
+	double gas;
+	
+	public Car(int number, String type, double gas) {
+		this.number = number;
+		this.type = type;
+		this.gas = gas;
+	}
+	run
+}
