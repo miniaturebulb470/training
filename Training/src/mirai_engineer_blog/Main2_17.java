fb749d971c6df921607ae52aba1404114bda4ea9
KAMIMURA SHUN <miniaturebulb470@gmail.com>
2025-02-06 15:42:53 +0900

    a

--- Training/src/mirai_engineer_blog/Main2_17.java
+++ Training/src/mirai_engineer_blog/Main2_17.java
@@ -3,7 +3,16 @@ package mirai_engineer_blog;
 public class Main2_17 {
 
 	public static void main(String[] args) {
-		
+        Car mycar = new Car(123, "ロミオ", 0.5);
+        try {
+            while(true) {
+                // 走行する
+                mycar.run();
+            }
+        } catch(GasolineException e) {
+            System.out.println(e.getMessage());
+        }
+
 	}
 
 }
@@ -22,5 +31,11 @@ class Car{
 		this.type = type;
 		this.gas = gas;
 	}
-	run
+	public void run() throws GasolineException {
+		if(this.gas < 0.1) {
+			throw new GasolineException("ナンバー"+this.number+"の"+this.type+"はガソリン不足のため走行できません。");
+		}
+		System.out.printf("ナンバー%dの%sは走行しています。%n",this.number,this.type);
+		this.gas -= 0.1;
+	}
 }
